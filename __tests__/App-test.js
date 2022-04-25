03f7b706d45879173a8c3c833a8e5c809947ef10
Alexis Figueroa <agfigueroa@teco.com.ar>
2022-04-25 17:23:22 -0300

    Unit Test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,29 @@
-/**
- * @format
- */
+import {
+  GET_FORECAST_DAY_ERROR,
+  GET_FORECAST_DAY_START,
+  GET_FORECAST_DAY_SUCCESS,
+  GET_FORECAST_ERROR,
+  GET_FORECAST_START,
+  GET_FORECAST_SUCCESS,
+  GET_NEW_FORECAST_ERROR,
+  GET_NEW_FORECAST_START,
+  GET_NEW_FORECAST_SUCCESS,
+} from '../src/const/actionForecastTypes';
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Test Type', () => {
+  test('Validar Types', () => {
+    expect(GET_FORECAST_START).toEqual('forecast:GET_FORECAST_START');
+    expect(GET_FORECAST_SUCCESS).toEqual('forecast:GET_FORECAST_SUCCESS');
+    expect(GET_FORECAST_ERROR).toEqual('forecast:GET_FORECAST_ERROR');
+    expect(GET_FORECAST_DAY_START).toEqual('forecast:GET_FORECAST_DAY_START');
+    expect(GET_FORECAST_DAY_SUCCESS).toEqual(
+      'forecast:GET_FORECAST_DAY_SUCCESS',
+    );
+    expect(GET_FORECAST_DAY_ERROR).toEqual('forecast:GET_FORECAST_DAY_ERROR');
+    expect(GET_NEW_FORECAST_START).toEqual('forecast:GET_NEW_FORECAST_START');
+    expect(GET_NEW_FORECAST_SUCCESS).toEqual(
+      'forecast:GET_NEW_FORECAST_SUCCESS',
+    );
+    expect(GET_NEW_FORECAST_ERROR).toEqual('forecast:GET_NEW_FORECAST_ERROR');
+  });
 });
